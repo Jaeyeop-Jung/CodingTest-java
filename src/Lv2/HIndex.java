18433a326881b3b569d905f8a59737dce1204703
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-15 01:03:55 +0900

    2022-12-14

---
+++ src/Lv2/HIndex.java
@@ -0,0 +1,28 @@
+package Lv2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class HIndex {
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int solution1 = solution.solution(new int[] {0, 3, 9, 10, 20});
+		System.out.println("solution1 = " + solution1);
+	}
+}
+
+class Solution {
+	public int solution(int[] citations) {
+		citations = Arrays.stream(citations).sorted().toArray();
+		int result = 0;
+		for (int i = 0; i < citations.length; i++) {
+			int h = citations.length - i;
+			if (citations[i] <= h) {
+				h = Math.min(h, citations[i]);
+			}
+			result = Math.max(result, h);
+		}
+		return result;
+	}
+}
---
+++ src/Lv2/괄호회전하기.java
@@ -0,0 +1,58 @@
+package Lv2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class 괄호회전하기 {
+	public boolean isCorrect(List<String> list) {
+		Stack<String> stack = new Stack<>();
+		for (String s : list) {
+			if (stack.isEmpty() && (s.equals(")") || s.equals("]") || s.equals("}"))) {
+				return false;
+			} else if (s.equals("(") || s.equals("[") || s.equals("{")) {
+				stack.push(s);
+			} else if (s.equals(")")) {
+				if (!stack.peek().equals("(")) {
+					return false;
+				}
+				stack.pop();
+			} else if (s.equals("}")) {
+				if (!stack.peek().equals("{")) {
+					return false;
+				}
+				stack.pop();
+			} else if (s.equals("]")) {
+				if (!stack.peek().equals("[")) {
+					return false;
+				}
+				stack.pop();
+			}
+		}
+		if (stack.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public int solution(String s) {
+		int result = 0;
+		for (int i = 0; i < s.length(); i++) {
+			List<String> collect = Arrays.stream(s.split("")).collect(Collectors.toList());
+			Collections.rotate(collect, i);
+			if (isCorrect(collect)) {
+				result += 1;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		괄호회전하기 괄호회전하기 = new 괄호회전하기();
+		int solution = 괄호회전하기.solution("(");
+		System.out.println("solution = " + solution);
+	}
+}
