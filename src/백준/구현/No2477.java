3de5c594fd8a9fc3de0bef44502259d96a20595a
Jaeyeop-Jung <jjy5349@naver.com>
2023-07-19 01:21:31 +0900

    2023-07-18

---
+++ src/백준/구현/No2477.java
@@ -0,0 +1,50 @@
+package 백준.구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class No2477 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		int k = Integer.parseInt(br.readLine());
+		int[][] arr = new int[6][];
+		int maxIdx = -1;
+		int maxLength = 0;
+		for (int i = 0; i < arr.length; i++) {
+			String[] temp = br.readLine().split(" ");
+			int t1 = Integer.parseInt(temp[0]);
+			int t2 = Integer.parseInt(temp[1]);
+			arr[i] = new int[] {t1, t2};
+			if (maxLength < t2) {
+				maxIdx = i;
+				maxLength = t2;
+			}
+		}
+
+		int maxGaro = maxLength;
+		int maxSeroIdx = 0;
+		if (maxIdx - 1 < 0) {
+			maxSeroIdx = 6 + maxIdx - 1;
+		} else {
+			maxSeroIdx = maxIdx - 1;
+		}
+		int maxSero = Math.max(arr[maxSeroIdx][1], arr[(maxIdx + 1) % 6][1]);
+
+		int res = maxGaro * maxSero;
+		int start = 0;
+		for (int i = 0; i < 6; i++) {
+			int next = (i + 1) % 6;
+			if (arr[i][0] == arr[(i + 2) % 6][0] && arr[next][0] == arr[(next + 2) % 6][0]) {
+				start = i;
+				break;
+			}
+		}
+
+		res -= arr[(start + 1) % 6][1] * arr[(start + 2) % 6][1];
+		System.out.println(res * k);
+	}
+}
---
+++ src/백준/그리디/No2785.java
@@ -0,0 +1,51 @@
+package 백준.그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class No2785 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		StringTokenizer sb = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(sb.nextToken());
+		}
+
+		List<Integer> list = Arrays.stream(arr)
+				.sorted()
+				.boxed()
+				.collect(Collectors.toList());
+
+		int res = 0;
+		ArrayDeque<Integer> q = new ArrayDeque<>(list);
+		while (2 < q.size()) {
+			res++;
+
+			Integer first = q.pollFirst();
+			Integer pop1 = q.pollLast();
+			Integer pop2 = q.pollLast();
+			q.addLast(pop1 + pop2);
+			if (0 < first - 1) {
+				q.addFirst(first - 1);
+			}
+		}
+
+		if (q.size() == 2) {
+			res++;
+		}
+
+		System.out.println(res);
+	}
+}
